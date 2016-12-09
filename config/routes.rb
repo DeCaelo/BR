3fd6113c7fc1d8e2978af7f56253d9d51b20ca08
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-09 09:39:05 +0100

    edit form, controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   namespace :admin do
-    resources :moderators, only: [:index]
+    resources :moderators, only: [:index, :edit, :update]
   end
 end
