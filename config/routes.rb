0a27148bb7c9886fbfaf0667d8b59086dbc5afb2
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-11 12:36:20 +0100

    visit any admin pages, pages should all be behind an authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   root to: 'pages#home'
 
+  get '/login' => 'admin/sessions#new'
+
   namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
   end
 end
